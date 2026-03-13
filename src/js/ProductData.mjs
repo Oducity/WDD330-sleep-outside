15dcf831fc93b31c851c2c54377a5fd46f2f6c8c
Oducity <henry.odumamwen@gmail.com>
2026-03-13 18:57:06 +0100

    Created Oducity--individual2 branch and completed the individual activity Dynamic product list

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -3,21 +3,21 @@ function convertToJson(res) {
     return res.json();
   } else {
     throw new Error("Bad Response");
-  }
-}
+  };
+};
 
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `../json/${this.category}.json`;
-  }
+    this.path = `../public/json/${this.category}.json`;
+  };
   getData() {
     return fetch(this.path)
       .then(convertToJson)
       .then((data) => data);
-  }
+  };
   async findProductById(id) {
     const products = await this.getData();
     return products.find((item) => item.Id === id);
-  }
-}
+  };
+};
