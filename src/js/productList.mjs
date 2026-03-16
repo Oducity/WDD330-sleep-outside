ee6c9feeffd474019da67c59cc8d6b49b8c823d8
Silverkike <enrique_guardado@outlook.es>
2026-03-16 00:26:00 -0600

    W02 Individual Activity - Dynamic Product List completed

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -2,17 +2,16 @@ import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
     return `<li class="product-card">
-        <a href="product_page/?product=${product.Id}">
-            <img src="${product.Image}" alt="image of ${product.Name}">
-            <h2>${product.Brand.Name}</h2>
-            <h3>${product.Name}</h3>
-            <p clas="product-card_price">${priduct.FinalPrice}</p>
-        </a>
-    </li>`;
-};
+      <a href="product_pages/?product=${product.Id}">
+          <img src="${product.Image}" alt="image of ${product.Name}">
+          <h2>${product.Brand.Name}</h2>
+          <h3>${product.Name}</h3>
+          <p class="product-card__price">${product.FinalPrice}</p>
+      </a>
+  </li>`;
+}
 
-
-export default class productList {
+export default class ProductList {
     constructor(category, dataSource, listElement) {
         this.category = category;
         this.dataSource = dataSource;
