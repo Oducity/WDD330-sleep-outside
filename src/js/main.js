e172a12213b21cf79cdb732458e575d2b93573e5
Silverkike <enrique_guardado@outlook.es>
2026-03-18 23:35:58 -0600

    Fix selector to match product-list in HTML

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,6 @@ import ProductList from "./productList.mjs";
 import productData from "./ProductData.mjs";
 
 const comingData = new productData("tents");
-const element = document.querySelector(".parent-list");
+const element = document.querySelector(".product-list");
 const productListInstance = new ProductList("Tents", comingData, element);
 productListInstance.init();
