d286d2d49646ae8247df9ad9c92f4e742469954e
Oducity <henry.odumamwen@gmail.com>
2026-03-21 16:20:01 +0100

    Imported alert class and created object of the same in the main.js file.

--- src/js/alert.mjs
+++ src/js/alert.mjs
@@ -1,44 +1,39 @@
 
 export default class alert{
-    //constructor(alertId, message, background, color) {
-    //    this.alertId = alertId;
-    //    this.message = message;
-    //    this.background = background;
-    //    this.color = color;
-    //}
-    //
-    async getAlertData() {
-    const main = document.querySelector("main");
-    try {
-        const alertAPI = "../public/json/alert.json";
-        const response = await fetch(alertAPI);
-        if (!response.ok) {
-            throw new Error(`Network response is not ok. Status: ${response.status}`);
-        }
-        const messages = await response.json();
-        const section = document.createElement("section");
-        section.classList.add = "alert-list";
-        messages.forEach((message) => {
-            const textP = document.createElement("p");
-            textP.innerText = message.text;
-            textP.style.backgroundColor = message.background;
-            textP.style.color = message.color;
-            section.appendChild(textP);
-        })
-        main.prepend(section);
-    } catch (error) {
-        const p = document.createElement("p");
-        p.classList.add = "error-message";
-        p.innerText = `Error displaying alert message! Error: ${error.message}`;
-        if (!main) {
-            console.error("<main> element not found");
-        } else {
-            main.prepend(p);
-        }
+    constructor(url) {
+        this.url = url;
     }
-    
-}
+    async getAlertData() {
+        const main = document.querySelector("main");
+        try {
+            //const alertAPI = "../public/json/alert.json";
+            const alertAPI = this.url;
+            const response = await fetch(alertAPI);
+            if (!response.ok) {
+                throw new Error(`Network response is not ok. Status: ${response.status}`);
+            }
+            const messages = await response.json();
+            const section = document.createElement("section");
+            section.classList.add = "alert-list";
+            messages.forEach((message) => {
+                const textP = document.createElement("p");
+                textP.innerText = message.text;
+                textP.style.backgroundColor = message.background;
+                textP.style.color = message.color;
+                section.appendChild(textP);
+            })
+            main.prepend(section);
+        } catch (error) {
+            const p = document.createElement("p");
+            p.classList.add = "error-message";
+            p.innerText = `Error displaying alert message! Error: ${error.message}`;
+            if (!main) {
+                console.error("<main> element not found");
+            } else {
+                main.prepend(p);
+            }
+        }    
+    }
 
-    
 }
 
