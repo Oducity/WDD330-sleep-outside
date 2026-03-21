cfd9b9898ed4a42b5db9ccf9ded9d3e7b855c1bb
Arom8-3-11 <rom24003@byui.edu>
2026-03-20 23:11:22 -0600

    fix of getdiscount

--- src/js/getDiscount.mjs
+++ src/js/getDiscount.mjs
@@ -1,19 +1,15 @@
-
-
-
 export function getDiscount(price) {
-    const xmasPerc = 3 / 100;
-    const easterPerc = 2 / 100;
-    const date = new Date();
-    const value = 1;
-    const month = date.toLocaleString("default", { month: "long" });
-    if (month === "March" && price >= 170) {
-        value = price * xmasPerc;
-    } else if (month === "April" && price >= 170) {
-      value = price * easterPerc;
-    } else {
-      value;
-    }
-    return value
+  const xmasPerc = 3 / 100;
+  const easterPerc = 2 / 100;
+  const date = new Date();
+  let value = 1;
+  const month = date.toLocaleString("default", { month: "long" });
 
+  if (month === "March" && price >= 170) {
+    value = price * xmasPerc;
+  } else if (month === "April" && price >= 170) {
+    value = price * easterPerc;
+  }
+
+  return value;
 }
