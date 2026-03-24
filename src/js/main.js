cd66275bee6b1f5280e3d09393e4b776d463f119
Oducity <henry.odumamwen@gmail.com>
2026-03-24 13:45:26 +0100

    Imported alert class from alert.mjs and created instance of the same in the main.js file.

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,13 @@
+import alert from "./alert.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductData.mjs";
-import alert from "./alert.mjs";
 
-const alertData = new alert("../public/json/alert.json");
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const productList = new ProductList("Tents", dataSource, element);
+const alertData = new alert("../public/json/alert.json");
 
 alertData.getAlertData();
 productList.init();
 import { loadHeaderFooter } from "./utils.mjs";
 loadHeaderFooter();
-
