85778f52ca10b5eff8d286909c9ded11bbf60814
Silverkike <enrique_guardado@outlook.es>
2026-03-26 23:33:45 -0600

    Fix main.js import and resolve merge issues

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import alert from "./alert.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
@@ -9,5 +10,4 @@ const alertData = new alert("../public/json/alert.json");
 
 alertData.getAlertData();
 productList.init();
-import { loadHeaderFooter } from "./utils.mjs";
 loadHeaderFooter();
